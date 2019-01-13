d173f6d56b3e890e645972262e277c091471a8db
Alyse Dunn <alyse.dunn@gmail.com>
2019-01-13 14:46:57 -0500

    Elastic Beanstalk updates.

--- Main/application.py
+++ Main/application.py
@@ -1,14 +1,26 @@
 from flask import Flask, render_template
+
 application = Flask(__name__)
+
+FLASK_ENV = 'production'
+FLASK_APP = 'Main/application.py'
+
 
 @application.route('/')
 def blog():
     return render_template('blog.html')
 
+
 @application.route('/flask-aws')
 def flask_aws():
     return render_template('flask_aws.html')
 
+
 @application.route('/single')
 def single():
     return render_template('single.html')
+
+
+if __name__ == "__main__":
+    application.debug = False
+    application.run()
