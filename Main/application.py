28a8ae8a71488932d705f10c411a351ef1411cff
Alyse Dunn <alyse.dunn@gmail.com>
2019-02-08 21:43:35 -0500

    Finishing touches to EB deploy post.

--- Main/application.py
+++ Main/application.py
@@ -16,14 +16,19 @@ def flask_aws():
     return render_template('flask_aws.html')
 
 
-@application.route('/single')
-def single():
-    return render_template('single.html')
-
-
 @application.route('/key-terms-and-definitions')
 def key_terms_and_definitions():
     return render_template('key_terms_and_definitions.html')
+
+
+@application.route('/novice-newcomer-to-professional-programmer')
+def novice_newcomer_to_professional_programmer():
+    return render_template('novice_newcomer_to_professional_programmer.html')
+
+
+@application.route('/single')
+def single():
+    return render_template('single.html')
 
 
 if __name__ == "__main__":
